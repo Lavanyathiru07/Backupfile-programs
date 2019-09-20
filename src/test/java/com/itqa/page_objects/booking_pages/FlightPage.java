b55438d054822be1debbfc987da946fc60b1ff85
M Sasikanth <sasikanth@tridentsqa.com>
2019-09-20 12:28:22 +0530

    Updated flight page page factory

--- src/test/java/com/itqa/page_objects/booking_pages/FlightPage.java
+++ src/test/java/com/itqa/page_objects/booking_pages/FlightPage.java
@@ -40,7 +40,7 @@ public class FlightPage extends BasePage {
         this.driver = DriverBase.getDriver();
         this.logger = Logger.getLogger(FlightPage.class);
         jse = (JavascriptExecutor) driver;
-        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 20), this);
     }
 
     public void selectDepFlight(int num, Itinerary itn) {
