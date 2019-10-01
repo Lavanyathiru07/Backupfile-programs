52936371edaa58a908ee9bf03431f81004a621ee
salex <salex@TSQA-L002.ittrident.local>
2019-10-01 15:38:04 +0530

    Added try catch in bundles page..

--- src/test/java/com/itqa/page_objects/booking_pages/BundlePage.java
+++ src/test/java/com/itqa/page_objects/booking_pages/BundlePage.java
@@ -7,6 +7,7 @@ import data.Itinerary;
 import framework.DriverBase;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,56 +20,58 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.List;
 
 public class BundlePage extends BasePage {
-    private Logger logger = null;
+	private Logger logger = null;
 
-    private WebDriver driver = null;
-    private JavascriptExecutor jse = null;
+	private WebDriver driver = null;
+	private JavascriptExecutor jse = null;
 
-    @FindBy(xpath = "//*[text()='Select your bundle']")
+	@FindBy(xpath = "//*[text()='Select your bundle']")
 	private WebElement bundleTitle;
 
-    @FindBy(xpath = "//*[@data-hook='select_select_button_2']")
-   	private WebElement selectAllegiantBonus;
-    
-    @FindBy(xpath = "//*[@data-hook='select_select_button_3']")
-   	private WebElement selectAllegiantTotal;
-    
-    @FindBy(xpath = "//*[@class='continue']")
-   	private WebElement continueButton;
-    
-    public BundlePage() {
-        this.driver = DriverBase.getDriver();
-        this.logger = Logger.getLogger(BundlePage.class);
-        jse = (JavascriptExecutor) driver;
-        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 20), this);
-    }
+	@FindBy(xpath = "//*[@data-hook='select_select_button_2']")
+	private WebElement selectAllegiantBonus;
 
-    public void selectBundle(Itinerary itn) throws Exception {
-    	
-    	Common.elementToBeClickable(driver, bundleTitle, "bundle Title");
-    	
-    	if(itn.getBundle().equalsIgnoreCase("AllegiantBonus")) {
-    		itn.setSeat(true);
-    		itn.setCarryOnBag(1);
-    		itn.setTripFlex(true);
-    		if(itn.getRoundTrip()) {
-    			itn.setSeatRT(true);
-    		}
-    		selectAllegiantBonus.click();
-    		
-    	}else if(itn.getBundle().equalsIgnoreCase("AllegiantTotal")) {
-    		itn.setSeat(true);
-    		itn.setCarryOnBag(1);
-    		itn.setCheckedBag(4);
-    		itn.setPriority("true");
-    		itn.setTripFlex(true);
-    		if(itn.getRoundTrip()) {
-    			itn.setSeatRT(true);
-    		}
-    		selectAllegiantTotal.click();
-    	}
-    	
-    	continueButton.click();
-    }
+	@FindBy(xpath = "//*[@data-hook='select_select_button_3']")
+	private WebElement selectAllegiantTotal;
+
+	@FindBy(xpath = "//*[@class='continue']")
+	private WebElement continueButton;
+
+	public BundlePage() {
+		this.driver = DriverBase.getDriver();
+		this.logger = Logger.getLogger(BundlePage.class);
+		jse = (JavascriptExecutor) driver;
+		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 20), this);
+	}
+
+	public void selectBundle(Itinerary itn) throws Exception {
+		try {
+			Common.elementToBeClickable(driver, bundleTitle, "bundle Title");
+
+			if (itn.getBundle().equalsIgnoreCase("AllegiantBonus")) {
+				itn.setSeat(true);
+				itn.setCarryOnBag(1);
+				itn.setTripFlex(true);
+				if (itn.getRoundTrip()) {
+					itn.setSeatRT(true);
+				}
+				selectAllegiantBonus.click();
+
+			} else if (itn.getBundle().equalsIgnoreCase("AllegiantTotal")) {
+				itn.setSeat(true);
+				itn.setCarryOnBag(1);
+				itn.setCheckedBag(4);
+				itn.setPriority("true");
+				itn.setTripFlex(true);
+				if (itn.getRoundTrip()) {
+					itn.setSeatRT(true);
+				}
+				selectAllegiantTotal.click();
+			}
+
+			continueButton.click();
+		} catch (NoSuchElementException e) {
+			logger.info("Bundles page is skipping");
+		}
+	}
 }
-
