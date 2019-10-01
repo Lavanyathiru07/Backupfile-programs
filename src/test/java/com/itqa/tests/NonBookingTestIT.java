d501995a02dcf77c7596209415b60a8f2ecd3a9c
pkasi <prasannavenkatesh@tridentsqa.com>
2019-10-01 15:30:38 +0530

    updated the code

--- src/test/java/com/itqa/tests/NonBookingTestIT.java
+++ src/test/java/com/itqa/tests/NonBookingTestIT.java
@@ -61,240 +61,240 @@ public class NonBookingTestIT extends DriverBase {
 		}
 	}
 
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "AIS: Search Decoupled Code For MX - Action Requests")
-//	@Story("AIS: Search Decoupled Code For MX - Action Requests")
-//	public void lookupActionRequest(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		G4PlusFlow nonBooking = new G4PlusFlow();
-//		nonBooking.lookupActionRequest();
-//	}
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "AIS: Search Coupled Code For MX - Aircraft Records")
-//	@Story("AIS: Search Coupled Code For MX - Aircraft Records")
-//	public void lookupAircraftRecordsPart(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.lookupAircraftRecordsPart();
-//		}
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "AIS: Run SPOE Reports - Line MX - MX Control - Reliabiliy - MX Records")
-//	@Story("AIS: Run SPOE Reports - Line MX - MX Control - Reliabiliy - MX Records")
-//	public void runSPOEreport(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//		G4PlusFlow nonBooking = new G4PlusFlow();
-//		nonBooking.runSPOEreport();
-//		}
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "AIS: Flight Information - Flight Following")
-//	@Story("AIS: Flight Information - Flight Following")
-//	public void verifyFlightFollowing(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//		G4PlusFlow nonBooking = new G4PlusFlow();
-//		nonBooking.verifyFlightFollowing();
-//		}
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "AIS: Access Inventory Maintenance")
-//	@Story("AIS: Access Inventory Maintenance")
-//	public void accessInventoryMX(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//		G4PlusFlow nonBooking = new G4PlusFlow();
-//		nonBooking.accessInventoryMX();
-//		 }
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//				description = "AIS: Access Print Manifest")
-//		@Story("AIS: Access Print Manifest")
-//		public void verifyPrintManifest(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//			
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.verifyPrintManifest();
-//			 }
-//	
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "AIS: Access Flight Schedule Maintenance")
-//	@Story("AIS: Access Flight Schedule Maintenance")
-//	public void verifyFlightScheduleMX(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//		G4PlusFlow nonBooking = new G4PlusFlow();
-//		nonBooking.verifyFlightScheduleMX();
-//		 }
-//	
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "AIS: Access Accounts Payable Maintenance - Look up Transaction")
-//	@Story("AIS: Access Accounts Payable Maintenance - Look up Transaction")
-//	public void lookupAccountsPayableMX(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		 
-//		G4PlusFlow nonBooking = new G4PlusFlow();
-//		nonBooking.lookupAccountsPayableMX();
-//		 }
-//	
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "AIS: Access Kayak Console")
-//	@Story("AIS: Access Kayak Console")
-//	public void accessKayakConsole(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//		G4PlusFlow nonBooking = new G4PlusFlow();
-//		nonBooking.accessKayakConsole();
-//		 }
-//	
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access STS")
-//	@Story("G4+: Access STS")
-//	public void accessSTS(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessSTS();
-//		}
-//		 }
-//	
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access ESP")
-//	@Story("G4+: Access ESP")
-//	public void accessESP(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessESP();
-//		}
-//	}
-//
-//
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access SVT")
-//	@Story("G4+: Access SVT")
-//	public void accessSVT(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessSVT();
-//		}
-//	}
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access CAR")
-//	@Story("G4+: Access CAR")
-//	public void accessCAR(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		 
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessCAR();
-//		}
-//	}
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access TF2")
-//	@Story("G4+: Access TF2")
-//	public void accessTF2(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		 
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessTF2();
-//		}else {
-//			//DriverBase.getDriver().close();
-//			throw new SkipException("Skipping Test Case due to Login application ");
-//		}
-//	}
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access RQ")
-//	@Story("G4+: Access RQ")
-//	public void accessRQ(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessRQ();
-//		}
-//	}
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access BAG")
-//	@Story("G4+: Access BAG")
-//	public void accessBAG(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessBAG();
-//		}else {
-//			//DriverBase.getDriver().close();
-//			throw new SkipException("Skipping Test Case due to Login application ");
-//		}
-//	}
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access PB2")
-//	@Story("G4+: Access PB2")
-//	public void accessPB2(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		 
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessPB2();
-//		}else {
-//			//DriverBase.getDriver().close();
-//			throw new SkipException("Skipping Test Case due to Login application ");
-//		}
-//	}
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access HOT")
-//	@Story("G4+: Access HOT")
-//	public void accessHOT(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessHOT();
-//		}
-//	}
-//	
-//	
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access ATL")
-//	@Story("G4+: Access ATL")
-//	public void accessATL(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessATL();
-//		}
-//		}
-//		
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access OFO")
-//	@Story("G4+: Access OFO")
-//	public void accessOFO(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessOFO();
-//			}else {
-//				//DriverBase.getDriver().close();
-//				throw new SkipException("Skipping Test Case due to Login  application ");
-//			}
-//	}
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "G4+: Access MOD")
-//	@Story("G4+: Access MOD")
-//	public void accessMOD(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		if (!env.contains("IN1") || !env.contains("IN2")) {
-//			G4PlusFlow nonBooking = new G4PlusFlow();
-//			nonBooking.accessMOD();
-//		}
-//	}
-//	
-//	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
-//			description = "Swap: Access Swap")
-//	@Story("Swap: Access Swap")
-//	public void accessSwap(Integer silo, Itinerary itn, ITestContext context, Method method) {
-//		G4PlusFlow nonBooking = new G4PlusFlow();
-//		nonBooking.accessSwap();
-//		 }
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "AIS: Search Decoupled Code For MX - Action Requests")
+	@Story("AIS: Search Decoupled Code For MX - Action Requests")
+	public void lookupActionRequest(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		G4PlusFlow nonBooking = new G4PlusFlow();
+		nonBooking.lookupActionRequest();
+	}
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "AIS: Search Coupled Code For MX - Aircraft Records")
+	@Story("AIS: Search Coupled Code For MX - Aircraft Records")
+	public void lookupAircraftRecordsPart(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.lookupAircraftRecordsPart();
+		}
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "AIS: Run SPOE Reports - Line MX - MX Control - Reliabiliy - MX Records")
+	@Story("AIS: Run SPOE Reports - Line MX - MX Control - Reliabiliy - MX Records")
+	public void runSPOEreport(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+		G4PlusFlow nonBooking = new G4PlusFlow();
+		nonBooking.runSPOEreport();
+		}
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "AIS: Flight Information - Flight Following")
+	@Story("AIS: Flight Information - Flight Following")
+	public void verifyFlightFollowing(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+		G4PlusFlow nonBooking = new G4PlusFlow();
+		nonBooking.verifyFlightFollowing();
+		}
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "AIS: Access Inventory Maintenance")
+	@Story("AIS: Access Inventory Maintenance")
+	public void accessInventoryMX(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+		G4PlusFlow nonBooking = new G4PlusFlow();
+		nonBooking.accessInventoryMX();
+		 }
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+				description = "AIS: Access Print Manifest")
+		@Story("AIS: Access Print Manifest")
+		public void verifyPrintManifest(Integer silo, Itinerary itn, ITestContext context, Method method) {
+			
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.verifyPrintManifest();
+			 }
+	
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "AIS: Access Flight Schedule Maintenance")
+	@Story("AIS: Access Flight Schedule Maintenance")
+	public void verifyFlightScheduleMX(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+		G4PlusFlow nonBooking = new G4PlusFlow();
+		nonBooking.verifyFlightScheduleMX();
+		 }
+	
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "AIS: Access Accounts Payable Maintenance - Look up Transaction")
+	@Story("AIS: Access Accounts Payable Maintenance - Look up Transaction")
+	public void lookupAccountsPayableMX(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		 
+		G4PlusFlow nonBooking = new G4PlusFlow();
+		nonBooking.lookupAccountsPayableMX();
+		 }
+	
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "AIS: Access Kayak Console")
+	@Story("AIS: Access Kayak Console")
+	public void accessKayakConsole(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+		G4PlusFlow nonBooking = new G4PlusFlow();
+		nonBooking.accessKayakConsole();
+		 }
+	
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access STS")
+	@Story("G4+: Access STS")
+	public void accessSTS(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessSTS();
+		}
+		 }
+	
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access ESP")
+	@Story("G4+: Access ESP")
+	public void accessESP(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessESP();
+		}
+	}
+
+
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access SVT")
+	@Story("G4+: Access SVT")
+	public void accessSVT(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessSVT();
+		}
+	}
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access CAR")
+	@Story("G4+: Access CAR")
+	public void accessCAR(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		 
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessCAR();
+		}
+	}
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access TF2")
+	@Story("G4+: Access TF2")
+	public void accessTF2(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		 
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessTF2();
+		}else {
+			//DriverBase.getDriver().close();
+			throw new SkipException("Skipping Test Case due to Login application ");
+		}
+	}
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access RQ")
+	@Story("G4+: Access RQ")
+	public void accessRQ(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessRQ();
+		}
+	}
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access BAG")
+	@Story("G4+: Access BAG")
+	public void accessBAG(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessBAG();
+		}else {
+			//DriverBase.getDriver().close();
+			throw new SkipException("Skipping Test Case due to Login application ");
+		}
+	}
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access PB2")
+	@Story("G4+: Access PB2")
+	public void accessPB2(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		 
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessPB2();
+		}else {
+			//DriverBase.getDriver().close();
+			throw new SkipException("Skipping Test Case due to Login application ");
+		}
+	}
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access HOT")
+	@Story("G4+: Access HOT")
+	public void accessHOT(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessHOT();
+		}
+	}
+	
+	
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access ATL")
+	@Story("G4+: Access ATL")
+	public void accessATL(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessATL();
+		}
+		}
+		
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access OFO")
+	@Story("G4+: Access OFO")
+	public void accessOFO(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessOFO();
+			}else {
+				//DriverBase.getDriver().close();
+				throw new SkipException("Skipping Test Case due to Login  application ");
+			}
+	}
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "G4+: Access MOD")
+	@Story("G4+: Access MOD")
+	public void accessMOD(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		if (!env.contains("IN1") || !env.contains("IN2")) {
+			G4PlusFlow nonBooking = new G4PlusFlow();
+			nonBooking.accessMOD();
+		}
+	}
+	
+	@Test(dataProvider = "NonBooking Use Cases", dataProviderClass = ItineraryDataProvider.class,
+			description = "Swap: Access Swap")
+	@Story("Swap: Access Swap")
+	public void accessSwap(Integer silo, Itinerary itn, ITestContext context, Method method) {
+		G4PlusFlow nonBooking = new G4PlusFlow();
+		nonBooking.accessSwap();
+		 }
 	
 
 	private void setUpTestContext(String description, ITestContext context, Itinerary itn) {
