486756cc131bb77077ed3b095b62f0719aec9ae6
M Sasikanth <sasikanth@tridentsqa.com>
2019-10-03 19:26:04 +0530

    updated environment class

--- src/main/java/com/itqa/Utils/Environment.java
+++ src/main/java/com/itqa/Utils/Environment.java
@@ -11,28 +11,25 @@ public class Environment {
 	public static Boolean shouldUpdateConfluenceStatusOnFailure = false;
 	private static Integer silo = -1;
 
-
-
 	private int[] stg = { 1, 2, 3 };
 	private int[] qa1 = { 1, 2 };
 	private int[] qa2 = { 1, 2 };
 	private int[] in1 = { 1 };
-	private int[] in2 = { 1 };
 	private int[] intl = { 1 };
 	private int[] trn = { 0 };
 
 
-	public static String getEnv() {
-		envUnderTest = System.getProperty("env");
-		if (envUnderTest == null) {
-			System.out.println("******* " + envUnderTest);
-			System.setProperty("env", "stg");
-			envUnderTest = System.getProperty("env");
-		}
-		return envUnderTest;
-	}
+    public static String getEnv() {
+        envUnderTest = System.getProperty("env");
+        if (envUnderTest == null) {
+            System.out.println("******* " + envUnderTest);
+            System.setProperty("env", "stg");
+            envUnderTest = System.getProperty("env");
+        }
+        return envUnderTest;
+    }
 
-	public List<Integer> getRandomSilo() {
+    public List<Integer> getRandomSilo() {
 		switch (getEnv()) {
 		case "stg":
 			Optional<Integer> optional = Arrays.stream(stg).boxed().skip((int) (stg.length * Math.random())).findAny();
@@ -55,8 +52,6 @@ public class Environment {
 			return stream(qa2).boxed().collect(Collectors.toList());
 		case "in1":
 			return stream(in1).boxed().collect(Collectors.toList());
-		case "in2":
-			return stream(in2).boxed().collect(Collectors.toList());
 		case "intl":
 			return stream(intl).boxed().collect(Collectors.toList());
 		case "trn":
