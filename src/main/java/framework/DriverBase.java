841ac5de07d8089a6793c2ff923c3fc10ccee105
salex <salex@TSQA-L002.ittrident.local>
2019-10-13 16:17:50 +0530

    Updated code

--- src/main/java/framework/DriverBase.java
+++ src/main/java/framework/DriverBase.java
@@ -67,10 +67,12 @@ public class DriverBase {
 
     @AfterSuite(alwaysRun = true)
     public static void closeDriverObjects() {
+    	DriverBase.getDriver().quit();
+    	/*
     	System.out.println("Inside closeDriverObjects");
         for (DriverFactory webDriverThread : webDriverThreadPool) {
             webDriverThread.quitDriver();
         }
         System.out.println("Finished closeDriverObjects");
-    }
+    */}
 }
