af9736d3f03e703584173ca325ad8800ffe665be
salex <salex@TSQA-L002.ittrident.local>
2019-10-13 14:52:55 +0530

    Updated code

--- src/main/java/listeners/RealTimeTestReport.java
+++ src/main/java/listeners/RealTimeTestReport.java
@@ -78,10 +78,10 @@ public class RealTimeTestReport extends DriverBase implements ITestListener {
         System.out.println("END Of Execution(TEST)->"+result.getName());
         System.out.println("**** on finish manifestid: " + result.getAttribute("manifestid"));
         
-        
+      /*  
         if (Environment.getCreateConfluenceSetting()) {
             UpdateConfluence page = new UpdateConfluence(result);
-        }
+        }*/
 
     }
 
