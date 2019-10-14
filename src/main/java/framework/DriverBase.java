6775ee1e72df4ba58eb5924f92fee96cdafa7323
M Sasikanth <sasikanth@tridentsqa.com>
2019-10-14 18:09:31 +0530

    Updated code for testing

--- src/main/java/framework/DriverBase.java
+++ src/main/java/framework/DriverBase.java
@@ -62,7 +62,7 @@ public class DriverBase {
         byte[] screenShotByteFile;
         screenShotByteFile = Screenshot.saveScreenshot( testResult.getName(), getDriver());   
         testResult.setAttribute("screenshot", screenShotByteFile);
-        getDriver().close();
+       // getDriver().close();
         driverThread.get().quitDriver();
        
 
