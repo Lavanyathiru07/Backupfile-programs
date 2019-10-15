114899400a2f4426ed9d87315f3bdc07937b71d6
salex <salex@TSQA-L002.ittrident.local>
2019-10-15 17:00:09 +0530

    Updated code

--- src/test/java/com/itqa/tests/TABookingTestIT.java
+++ src/test/java/com/itqa/tests/TABookingTestIT.java
@@ -94,7 +94,6 @@ public class TABookingTestIT extends DriverBase {
 				|| ((env.contains("qa1") || env.contains("qa2")) && (silo == 2))
 				|| ((env.contains("in1") || env.contains("in2")) && (silo == 1)) || (env.contains("trn") && (silo == 1))
 				|| (env.contains("nddprd") && ((silo == 1) || (silo == 2) || (silo == 3)))) {
-
 			setUpTestContext(silo, method.getAnnotation(Story.class).value(), context, itn);
 			TABookingFlow booking = new TABookingFlow();
 			generateBooking(itn, silo, context);
