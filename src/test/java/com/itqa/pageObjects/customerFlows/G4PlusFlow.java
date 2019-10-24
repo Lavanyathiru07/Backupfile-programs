7b7f7e4ea9c090ce6f9e1e9e7208e1f7bd6af5f6
M Sasikanth <sasikanth@tridentsqa.com>
2019-10-24 16:17:27 +0530

    Updated code for prod testing

--- src/test/java/com/itqa/pageObjects/customerFlows/G4PlusFlow.java
+++ src/test/java/com/itqa/pageObjects/customerFlows/G4PlusFlow.java
@@ -142,6 +142,8 @@ public class G4PlusFlow extends BasePage {
 				/*DriverBase.getDriver().get(URLS.G4PLUS.getUrl(Environment.getEnv(), 0));
 				G4PlusLoginPage.g4plusLogin(false);*/
 			} else if (Environment.getEnv().contains("prod")) {
+				System.out.println(System.getProperty("username"));
+				System.out.println(System.getProperty("password"));
 				DriverBase.getDriver().get(URLS.G4PLUS.getUrl(Environment.getEnv(), 0));
 				G4PlusLoginPage.g4plusLogin(false);
 			} else if (Environment.getEnv().contains("nddprd")){
