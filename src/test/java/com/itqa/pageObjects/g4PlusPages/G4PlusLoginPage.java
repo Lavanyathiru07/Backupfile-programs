f382004626045a52fb19c5e9ffd4dede5b3358d9
Prasanna Kasi <prasannavenkatesh@tridentsqa.com>
2019-11-06 00:09:18 -0800

    updated the NDDPRD

--- src/test/java/com/itqa/pageObjects/g4PlusPages/G4PlusLoginPage.java
+++ src/test/java/com/itqa/pageObjects/g4PlusPages/G4PlusLoginPage.java
@@ -23,7 +23,6 @@ public class G4PlusLoginPage extends BasePage {
 	private WebDriver driver = null;
 	private JavascriptExecutor jse = null;
 
-	private String username = "Y2hhcm5raWp0YXdhcnVzaC5hdQ==";
 	private String stationUsername = "Q2hhbmF0YW4uQ2hhcm4udGVzdA==";
 	private String password = "QFNkMTUwNDEyMzQ1";
 
@@ -60,14 +59,8 @@ public class G4PlusLoginPage extends BasePage {
 			if (!station) {
 				if (Environment.getEnv().contains("prod")||Environment.getEnv().contains("nddprd")) {
 					userNameField.sendKeys(System.getProperty("username"));
-					Thread.sleep(500);
 					passwordField.sendKeys(System.getProperty("password"));
-					Thread.sleep(500);
 				} else {
-					userNameField.sendKeys(new String(Base64.getDecoder().decode(username)));
-					passwordField.sendKeys(new String(Base64.getDecoder().decode(password)));
-				}
-			} else {
 				userNameField.sendKeys(new String(Base64.getDecoder().decode(stationUsername)));
 				passwordField.sendKeys(new String(Base64.getDecoder().decode(password)));
 			}
