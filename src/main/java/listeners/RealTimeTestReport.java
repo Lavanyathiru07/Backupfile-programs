ca19125acfa535515f248f969f8c132f32015ec8
M Sasikanth <sasikanth@tridentsqa.com>
2019-11-14 18:13:39 +0530

    Updated Report in BAT2.0

--- src/main/java/listeners/RealTimeTestReport.java
+++ src/main/java/listeners/RealTimeTestReport.java
@@ -46,7 +46,7 @@ public class RealTimeTestReport extends DriverBase implements ITestListener {
 		GeneralUtils.writeToFile("passedTests.html",
 				"<tr><td align=\"left\">" + testResultContext.description
 						+ "</td><td align=\"center\"><font color='green'>PASSED</font></td><td>" + testResultContext.itn
-						+ "</td><td>"
+						+ "</td><td></td><td>"
 						+ "<a href=\"javascript:setImageVisible('show'," + testResultContext.currentSilo
 						+ ");\">show image</a>" + "<img id='screenshotId" + testResultContext.currentSilo + "' "
 						+ "style='display:inline' height=\"40%\" width=\"auto\" src='" + base64Screenshot + "'/>"
