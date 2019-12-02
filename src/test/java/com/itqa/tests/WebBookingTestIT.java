cb033242113a875894518e00fd03f076a8633307
M Sasikanth <sasikanth@tridentsqa.com>
2019-12-02 11:56:44 +0530

    Updated DHS clear code in BAT2.0

--- src/test/java/com/itqa/tests/WebBookingTestIT.java
+++ src/test/java/com/itqa/tests/WebBookingTestIT.java
@@ -8,6 +8,7 @@ import com.itqa.Utils.Environment;
 import com.itqa.Utils.URLS;
 import com.itqa.pageObjects.customerFlows.BookingFlow;
 
+import clearingITN.DHSClear;
 import data.*;
 import io.qameta.allure.Story;
 import listeners.TestResultContext;
@@ -125,6 +126,12 @@ public class WebBookingTestIT extends DriverBase {
 			BookingFlow booking = generateBooking(itn, silo, context, WITHOUTACCOUNT);
 
 			Assert.assertNotNull(itn.getItn(), "ITN could not be created");
+			try {
+				DHSClear doDHS = new DHSClear();
+				doDHS.dhs(env, itn.getItn());
+			}catch(Exception e) {
+				log.info("error getting while clear the DHS");
+			}
 			
 			
 
