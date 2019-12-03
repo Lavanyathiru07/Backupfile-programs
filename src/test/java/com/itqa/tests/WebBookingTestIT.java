05b233193eb98c094c7fb010345c2326d12e6c4e
M Sasikanth <sasikanth@tridentsqa.com>
2019-12-03 20:42:50 +0530

    Updated DB query

--- src/test/java/com/itqa/tests/WebBookingTestIT.java
+++ src/test/java/com/itqa/tests/WebBookingTestIT.java
@@ -115,9 +115,9 @@ public class WebBookingTestIT extends DriverBase {
 			}
 			if (flightAvailService == 0 && paymentService == 0) {
 			try {
-				//setEarlyMarketCities(itn);
-				itn.setDepartureCity("FAT");
-				itn.setDestinationCity("LAS");
+				setEarlyMarketCities(itn);
+				//itn.setDepartureCity("FAT");
+				//itn.setDestinationCity("LAS");
 			} catch (Exception e) {
 				log.info("Error while getting the early flight. So, It's executing with default city pair(FAT-LAS)");
 				itn.setDepartureCity("FAT");
