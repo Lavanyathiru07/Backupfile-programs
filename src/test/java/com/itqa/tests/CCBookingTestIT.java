9337f22c233e837050bcebdcb602c960f2bb7595
M Sasikanth <sasikanth@tridentsqa.com>
2019-12-12 14:36:20 +0530

    Updated code for testing PROD CC test cases

--- src/test/java/com/itqa/tests/CCBookingTestIT.java
+++ src/test/java/com/itqa/tests/CCBookingTestIT.java
@@ -162,7 +162,9 @@ public class CCBookingTestIT extends DriverBase {
 			DriverBase.getDriver().get(URLS.CC.getUrl(System.getProperty("awsenv"), silo));
 		} else if (env.contains("nddprd")) {
 			DriverBase.getDriver().get(URLS.G4PLUS.getUrl(Environment.getEnv(), 0));  
-		} else {
+		} else if (env.contains("prod")) {
+			DriverBase.getDriver().get(URLS.G4PLUS.getUrl(Environment.getEnv(), 0));  
+		}else {
 			DriverBase.getDriver().get(URLS.G4PLUSTOKEN.getUrl(env, 0));
 			DriverBase.getDriver().get(URLS.CC.getUrl(env, silo));
 		}
