68131ab7557131d172d19e9e67b26aa6efb71593
Pavan KP <pavan.kp@p10.com>
2020-01-02 18:41:16 +0530

    updated to get voucher number also along with ITN

--- src/test/java/com/itqa/tests/WebBookingTestIT.java
+++ src/test/java/com/itqa/tests/WebBookingTestIT.java
@@ -230,22 +230,6 @@ public class WebBookingTestIT extends DriverBase {
 		if (((env.contains("qa1") || env.contains("qa2") || env.contains("stg") || env.contains("aws")) && (silo == 1))
 				|| (env.contains("prod") && (silo == 3))) {
 			setUpTestContext(silo, "silo" + silo + " " + method.getAnnotation(Story.class).value(), context, itn);
-			cat.createTest(itn.getDescription(), "BAT 2.0", testId.get());
-			Properties props = new Properties();
-			props.setProperty("log4j.appender.file", "org.apache.log4j.RollingFileAppender");
-			props.setProperty("log4j.appender.file.maxFileSize", "100MB");
-			props.setProperty("log4j.appender.file.maxBackupIndex", "0");
-			props.setProperty("log4j.appender.file.File", System.getProperty("user.dir") + "/target/" + itn.getDescription()
-			+ Thread.currentThread().getId() + ".log");
-			props.setProperty("log4j.appender.file.threshold", "DEBUG");
-			props.setProperty("log4j.appender.file.Append", "false");
-			props.setProperty("log4j.appender.file.layout", "org.apache.log4j.PatternLayout");
-			props.setProperty("log4j.appender.file.layout.ConversionPattern", "%m%n");
-			props.setProperty("log4j.logger." + "Thread" + Thread.currentThread().getId(), "DEBUG, file");
-
-			PropertyConfigurator.configure(props);
-			desc.set(itn.getDescription());
-			itinerary = itn.getItn();
 			logger.get().info("Accout creation booking started");
 			desc.set(itn.getDescription());
 
@@ -267,6 +251,22 @@ public class WebBookingTestIT extends DriverBase {
 			}
 			updateTextContext(itn, context);
 			booking.WWWRefundAndCancelItn(itn.getItn(), itn);
+			cat.createTest(itn.getDescription(), "BAT 2.0", testId.get());
+			Properties props = new Properties();
+			props.setProperty("log4j.appender.file", "org.apache.log4j.RollingFileAppender");
+			props.setProperty("log4j.appender.file.maxFileSize", "100MB");
+			props.setProperty("log4j.appender.file.maxBackupIndex", "0");
+			props.setProperty("log4j.appender.file.File", System.getProperty("user.dir") + "/target/" + itn.getDescription()
+			+ Thread.currentThread().getId() + ".log");
+			props.setProperty("log4j.appender.file.threshold", "DEBUG");
+			props.setProperty("log4j.appender.file.Append", "false");
+			props.setProperty("log4j.appender.file.layout", "org.apache.log4j.PatternLayout");
+			props.setProperty("log4j.appender.file.layout.ConversionPattern", "%m%n");
+			props.setProperty("log4j.logger." + "Thread" + Thread.currentThread().getId(), "DEBUG, file");
+
+			PropertyConfigurator.configure(props);
+			desc.set(itn.getDescription());
+			itinerary = itn.getItn();
 		} else {
 			if (flightAvailService != 0) {
 				itn.setItn(flightAvailErrorMsg);
