ce93652459cfea40900e8b8e539f24948d1ae399
M Sasikanth <sasikanth@tridentsqa.com>
2020-02-05 19:36:24 +0530

    updated data provider code

--- src/main/java/data/ItineraryDataProvider.java
+++ src/main/java/data/ItineraryDataProvider.java
@@ -72,8 +72,8 @@ public class ItineraryDataProvider {
             for (Integer silo: silos) {
                 Itinerary itnRoundTrip = new ItineraryBuilder()
                         .withScenario(method.getName())
-                        .withDepartureCity("CVG")
-                        .withDestinationCity("SFB")
+                        .withDepartureCity("LAS")
+                        .withDestinationCity("FAT")
                         .withRoundTrip(true)
                         .withCarryOnBag(1)
                         .withCheckedBag(2)
@@ -116,8 +116,8 @@ public class ItineraryDataProvider {
         	for (Integer silo: silos) {
                 Itinerary itnRoundTrip = new ItineraryBuilder()
                         .withScenario(method.getName())
-                        .withDepartureCity("CVG")
-                        .withDestinationCity("SFB")
+                        .withDepartureCity("BLI")
+                        .withDestinationCity("LAS")
                         .withHotel(true)
                         .withVehicle(true)
                         .withSsr("PPOC")
