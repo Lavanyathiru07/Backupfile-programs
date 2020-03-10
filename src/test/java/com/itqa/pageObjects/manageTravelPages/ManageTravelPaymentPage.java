ea72633b344d38bede7a4f872c663e77c1406914
fv2s <rene.flores-torres@allegiantair.com>
2020-03-09 17:56:36 -0700

    increasing timer

--- src/test/java/com/itqa/pageObjects/manageTravelPages/ManageTravelPaymentPage.java
+++ src/test/java/com/itqa/pageObjects/manageTravelPages/ManageTravelPaymentPage.java
@@ -105,7 +105,7 @@ public class ManageTravelPaymentPage {
             new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(cardNoField));
         }catch (WebDriverException e ){
             try {
-                Thread.sleep(2000);
+                Thread.sleep(2500);
             } catch (InterruptedException ex) {
 
             }
