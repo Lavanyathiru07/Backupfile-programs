b9887d050f706c64e682c47f4b45bcb3ae4d1cb5
fv2s <rene.flores-torres@allegiantair.com>
2020-03-11 14:58:05 -0700

    testing

--- src/test/java/com/itqa/pageObjects/g4PlusPages/MOD.java
+++ src/test/java/com/itqa/pageObjects/g4PlusPages/MOD.java
@@ -776,9 +776,13 @@ public class MOD extends BasePage {
 	}
 
 	public void stationUncheckPax(String pnr, Itinerary itn) {
-		DriverBase.getDriver().get(URLS.G4PLUS.getUrl(Environment.getEnv(), 0));
-		logger.info("Connecting to:  "+ URLS.G4PLUS.getUrl(Environment.getEnv(), 0));
-		g4LoginPage.g4plusLogin(true);
+		try {
+			DriverBase.getDriver().get(URLS.G4PLUS.getUrl(Environment.getEnv(), 0));
+			logger.info("Connecting to:  " + URLS.G4PLUS.getUrl(Environment.getEnv(), 0));
+			g4LoginPage.g4plusLogin(true);
+		}catch (Exception e){
+			System.out.println("Error "+e);
+		}
 		Set<String> curTab = driver.getWindowHandles();
 		driver = DriverBase.getDriver();
 		driver.navigate().refresh();
