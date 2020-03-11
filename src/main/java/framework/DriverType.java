b80aaf7d3a56ee0d9e31066a6238376711efccd1
fv2s <rene.flores-torres@allegiantair.com>
2020-03-10 18:59:17 -0700

    update

--- src/main/java/framework/DriverType.java
+++ src/main/java/framework/DriverType.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 
 public enum DriverType implements DriverSetup {
 
-
 	FIREFOX {
 		public RemoteWebDriver getWebDriverObject(DesiredCapabilities capabilities) {
 			// todo: remove this path
@@ -99,4 +98,5 @@ public enum DriverType implements DriverSetup {
 	};
 
 	public final static boolean HEADLESS = Boolean.getBoolean("headless");
+
 }
