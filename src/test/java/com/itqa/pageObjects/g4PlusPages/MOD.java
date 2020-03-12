310a35ce7cb4c38608d85041fa128d81cf61cfd5
fv2s <rene.flores-torres@allegiantair.com>
2020-03-12 14:19:03 -0700

    increase time for menu error case

--- src/test/java/com/itqa/pageObjects/g4PlusPages/MOD.java
+++ src/test/java/com/itqa/pageObjects/g4PlusPages/MOD.java
@@ -771,7 +771,7 @@ public class MOD extends BasePage {
 		try {
 			driver = DriverBase.getDriver();
 			for( int index =0; index<5; index++) {
-				Thread.sleep(3500);
+				Thread.sleep(5500);
 				if (driver.findElement(By.xpath("//*[contains(text(),'Menu Error')]")).isDisplayed()) {
 					driver.navigate().refresh();
 				} else {
@@ -796,7 +796,7 @@ public class MOD extends BasePage {
 		try {
 			driver = DriverBase.getDriver();
 			for( int index =0; index<5; index++) {
-				Thread.sleep(3500);
+				Thread.sleep(5500);
 				if (driver.findElement(By.xpath("//*[contains(text(),'Menu Error')]")).isDisplayed()) {
 					driver.navigate().refresh();
 				} else {
@@ -821,12 +821,17 @@ public class MOD extends BasePage {
 		
 		Set<String> curTab = driver.getWindowHandles();
 		try {
-			Thread.sleep(2500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		driver = DriverBase.getDriver();
-		driver.navigate().refresh();
+			driver = DriverBase.getDriver();
+			for( int index =0; index<5; index++) {
+				Thread.sleep(5500);
+				if (driver.findElement(By.xpath("//*[contains(text(),'Menu Error')]")).isDisplayed()) {
+					driver.navigate().refresh();
+				} else {
+					break;
+				}
+			}
+
+		}catch (Exception e ){}
 		g4MenuPage.selectMOD(itn);
 		GeneralUtils.switchNextTab(driver, curTab);
 
