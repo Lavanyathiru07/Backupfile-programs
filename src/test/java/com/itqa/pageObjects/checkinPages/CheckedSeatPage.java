f21032f9f0cf92abe06fcbcdeff332b444f41951
fv2s <rene.flores-torres@allegiantair.com>
2020-03-13 15:42:16 -0700

    checking if user is prompted error in seat selection

--- src/test/java/com/itqa/pageObjects/checkinPages/CheckedSeatPage.java
+++ src/test/java/com/itqa/pageObjects/checkinPages/CheckedSeatPage.java
@@ -82,6 +82,15 @@ public class CheckedSeatPage extends BasePage {
             }
             logger.info("Click Continue");
         }
+
+        try {
+            if (driver.findElement(By.xpath("//h2[contains(text(), 'sorry')]")).isDisplayed()) {
+                driver.findElement(By.xpath("//span[contains(text(),'Continue')]/parent::button")).sendKeys(Keys.RETURN);
+                chooseSeat();
+                jse.executeScript("arguments[0].click();", continueButton);
+                jse.executeScript("arguments[0].click();", yesContinueButton);
+            }
+        }catch (Exception e ){}
     }
 
 
