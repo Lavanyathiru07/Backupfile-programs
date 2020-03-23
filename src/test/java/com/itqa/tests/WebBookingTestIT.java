80b38a0017a4e2a485701a059f0b5cd6eb4f7440
M Sasikanth <sasikanth@tridentsqa.com>
2020-03-23 21:48:47 +0530

    QAA-624 Implemented Bundle test cases in all lower environments

--- src/test/java/com/itqa/tests/WebBookingTestIT.java
+++ src/test/java/com/itqa/tests/WebBookingTestIT.java
@@ -304,7 +304,7 @@ public class WebBookingTestIT extends DriverBase {
 			testnum++;
 		}
 
-		if ((env.contains("qa1")||env.contains("qa2")||env.contains("stg")) && (silo == 1)) {
+		if ((env.contains("in1") ||env.contains("in2")||env.contains("qa1")||env.contains("qa2")||env.contains("stg"))&& (silo == 1)) {
 			
 			setUpTestContext(silo, "silo" + silo + " " + method.getAnnotation(Story.class).value(), context,
 					itn);
@@ -366,7 +366,7 @@ public class WebBookingTestIT extends DriverBase {
 			testnum++;
 		}
 
-		if (env.contains("stg") && (silo == 1)) {
+		if ((env.contains("in1") ||env.contains("in2")||env.contains("qa1")||env.contains("qa2")||env.contains("stg"))&& (silo == 1)) {
 			
 			setUpTestContext(silo, "silo" + silo + " " + method.getAnnotation(Story.class).value(), context,
 					itn);
