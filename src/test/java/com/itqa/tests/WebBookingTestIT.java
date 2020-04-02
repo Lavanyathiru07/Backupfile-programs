69be55283e852038fbf8327e276d862556a95246
vvelayutham <vvelayutham@TSQA-L081.ittrident.local>
2020-04-02 13:12:11 +0530

    Updated the env conditions

--- src/test/java/com/itqa/tests/WebBookingTestIT.java
+++ src/test/java/com/itqa/tests/WebBookingTestIT.java
@@ -306,7 +306,7 @@ public class WebBookingTestIT extends DriverBase {
 		}
 
 		if ((env.contains("in1") ||env.contains("in2")||env.contains("qa1")||env.contains("qa2")||env.contains("stg")
-				||env.contains("trn")||env.contains("nddprd")||env.contains("aws")||env.contains("sb1"))&& (silo == 1)) {
+				||env.contains("trn")||env.contains("nddprd")||env.contains("aws")||env.contains("sb1")||env.contains("prod"))&& (silo == 1)) {
 			
 			setUpTestContext(silo, "silo" + silo + " " + method.getAnnotation(Story.class).value(), context,
 					itn);
@@ -369,7 +369,7 @@ public class WebBookingTestIT extends DriverBase {
 		}
 
 		if ((env.contains("in1") ||env.contains("in2")||env.contains("qa1")||env.contains("qa2")||env.contains("stg")
-				||env.contains("trn")||env.contains("nddprd")||env.contains("aws")||env.contains("sb1"))&& (silo == 1)) {
+				||env.contains("trn")||env.contains("nddprd")||env.contains("aws")||env.contains("sb1")||env.contains("prod"))&& (silo == 1)) {
 			
 			setUpTestContext(silo, "silo" + silo + " " + method.getAnnotation(Story.class).value(), context,
 					itn);
