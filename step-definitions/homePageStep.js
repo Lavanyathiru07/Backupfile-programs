b6d02ba84a60b532e86b0d35a55638e135c6c249
Sandhiya K <sandhiya.k@allegiantair.com>
2025-01-29 19:30:46 +0530

    UI Implementation for RT Scenario

--- step-definitions/homePageStep.js
+++ step-definitions/homePageStep.js
@@ -64,8 +64,9 @@ When(/^I am on MT landing page I choose the departure date "(.+)" days from curr
     if (process.env.ENV.includes("prod") || process.env.tag.includes("prod")) {
         departureDate = await homePage.chooseDepartingDate(18);
     } else {
-        await browser.setWindowSize(2700, 2700)
+        // await browser.setWindowSize(2800, 2800)
         departureDate = await homePage.chooseDepartingDate(number);
+        
     }
 });
 
