c24d2c64a019bcc2d8a75b26a27d2ce59cdc7f5f
Sandhiya K <sandhiya.k@allegiantair.com>
2025-03-10 09:07:43 +0530

    adding features

--- wdio.conf.js
+++ wdio.conf.js
@@ -37,7 +37,7 @@ if (target_browser) {
 
 exports.config = _.extend({
   specs: [
-    './features/**/RT_Domestic.feature'
+    './features/**/*.feature'
   ],
   exclude: [
     // 'path/to/excluded/files'
