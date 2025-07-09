155e8bbefcae21e4449d05a88a14b6d22b48bdf6
zq8s_algt <krishnadas.balachandran@allegiantair.com>
2025-07-09 18:28:07 +0530

    updated maxInstance to 6

--- wdio.conf.js
+++ wdio.conf.js
@@ -46,7 +46,7 @@ exports.config = _.extend({
   port: 443,
   path: '/wd/hub',
   protocol: 'https',
-  maxInstances: 5,
+  maxInstances: 6,
   capabilities: browserConfig,
   // capabilities: [{
   //   browserName: 'chrome',
