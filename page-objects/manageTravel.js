26cc66b4c25c091bddb1a1c49eee463996e74364
Rithik Sai Kancharla <saikancharla.r+algt@allegiantair.com>
2025-08-06 00:19:59 +0530

    Removed the if condition for ITN cancellation  as it is specifically verifying for prod

--- page-objects/manageTravel.js
+++ page-objects/manageTravel.js
@@ -710,9 +710,9 @@ class Managetravel {
     }
 
     async clickCancelFlight() {
-        await actions.waitForDisplayed(cancelFlight, 'cancelFlight', 30000)
-        await actions.waitForClickable(cancelFlight, 'cancelFlight')
-        await actions.click(cancelFlight, 'cancelFlight Button')
+        await actions.waitForDisplayed(cancelButton, 'cancelFlight', 30000)
+        await actions.waitForClickable(cancelButton, 'cancelFlight')
+        await actions.click(cancelButton, 'cancelFlight Button')
     }
 
     async clickCancelReason() {
