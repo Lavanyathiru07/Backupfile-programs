82debba09872e4e2d59c39f2b2437070386d6a71
Aswathi Prathap <Aswathi.Prathap@allegiantair.com>
2025-11-06 11:32:19 +0530

    feat: updated

--- prova-ui/cucumber.cjs
+++ prova-ui/cucumber.cjs
@@ -1,8 +1,7 @@
 module.exports = {
     default: {
         paths: ['test/features/navitaire/*.feature'],
-        import: ['cat-hooks/cat-playwright-hooks.js'],
-        require: ['test/step-definitions/*.js'],
+        import: ['cat-hooks/cat-playwright-hooks.js', 'test/step-definitions/*.js'],
         format: ['html:./cucumber-report.html', 'json:report.json']
     }
 };
