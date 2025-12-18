1be266022c7f1b23d561fc0a4788bab6e14a65c2
Aswathi Prathap <Aswathi.Prathap@allegiantair.com>
2025-12-18 17:23:38 +0530

    feat: updated

--- prova-ui/test/step-definitions/bagsPageStep.js
+++ prova-ui/test/step-definitions/bagsPageStep.js
@@ -5,26 +5,26 @@ Then(/^I am on Bags page I select CarryOn "([^"]*)"$/, async function (params) {
     await this.BagsPage.selectCarryOnBagByParams(params)
 })
 
-Then(/^I am on Bags page I select Checked Bag "([^"]*)"$/, async function (params)  {
+Then(/^I am on Bags page I select Checked Bag "([^"]*)"$/, async function (params) {
     await this.BagsPage.selectCheckedBagByParams(params)
 })
 
-Then(/^I am on Bags page I select tripflex "([^"]*)"$/, async function (params)  {
+Then(/^I am on Bags page I select tripflex "([^"]*)"$/, async function (params) {
     await this.BagsPage.selectTripflex(params)
 })
 
-Then(/^I am on Bags page I select priority "([^"]*)"$/, async function (params)  {
+Then(/^I am on Bags page I select priority "([^"]*)"$/, async function (params) {
     await this.BagsPage.selectPriorityByParams(params)
 })
 
-Then(/^I am on Bags page I click continue button$/, async function ()  {
+Then(/^I am on Bags page I click continue button$/, { timeout: 180 * 10000 }, async function () {
     await this.BagsPage.clickContinueButton()
 })
 
-Then(/^I am on Bags page I click continue button without selecting bags$/, async function ()  {
+Then(/^I am on Bags page I click continue button without selecting bags$/, async function () {
     await this.BagsPage.clickContinueButton();
 });
 
-Then(/^I am on Bags page I select pet in passenger cabin (.+)$/, async function (params)  {
-	await this.BagsPage.selectPetInCabinByParams(params);
+Then(/^I am on Bags page I select pet in passenger cabin (.+)$/, async function (params) {
+    await this.BagsPage.selectPetInCabinByParams(params);
 });
