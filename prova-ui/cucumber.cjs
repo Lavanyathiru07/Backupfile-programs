9483a443bcaefd8abac3ee4826c3bac3dcbb2e6e
Aswathi Prathap <Aswathi.Prathap@allegiantair.com>
2026-01-15 14:42:55 +0530

    feat: updated

--- prova-ui/cucumber.cjs
+++ prova-ui/cucumber.cjs
@@ -1,6 +1,9 @@
+// Set environment variables before exporting configuration
+process.env.appEnv = 'https://www-cxj2512070.apps.swe-qat.aws.allegiantair.com';
+
 module.exports = {
     default: {
-        paths: ['test/features/navitaire/*.feature'],
+        paths: ['test/features/navitaire/ATL_E2E.feature'],
         import: ['cat-hooks/cat-playwright-hooks.js', 'test/step-definitions/*.js'],
         format: ['html:./cucumber-report.html', 'json:report.json']
     }
