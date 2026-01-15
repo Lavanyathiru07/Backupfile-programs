73f1a23df9aa9407f23e3568c1eba1fcf73e1dd0
Aswathi Prathap <Aswathi.Prathap@allegiantair.com>
2026-01-15 14:48:24 +0530

    feat: updated

--- prova-ui/cucumber.cjs
+++ prova-ui/cucumber.cjs
@@ -3,7 +3,7 @@ process.env.appEnv = 'https://www-cxj2512070.apps.swe-qat.aws.allegiantair.com';
 
 module.exports = {
     default: {
-        paths: ['test/features/navitaire/ATL_E2E.feature'],
+        paths: ['test/features/navitaire/*.feature'],
         import: ['cat-hooks/cat-playwright-hooks.js', 'test/step-definitions/*.js'],
         format: ['html:./cucumber-report.html', 'json:report.json']
     }
